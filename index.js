b799996a5903c91381bb2ee33fbcdd594f04f2f5
SergeyKurshin <mr.kurshin@mail.ru>
2021-02-07 12:53:47 +0800

    Change status

--- index.js
+++ index.js
@@ -130,7 +130,6 @@ function toggleTask() {
                 }  else {
                     li.classList.remove('completed');
                     task.completed = false;
-
                 };
             };
     };
