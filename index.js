bf02b47946e769a496de0095b0b10ca942cdb069
SergeyKurshin <mr.kurshin@mail.ru>
2021-02-15 18:31:46 +0800

    making an edit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const task = {
     completed: true
 };
 let tasksList = [
-    { id: "1", text: "выучить html", completed: true },
-    { id: "2", text: "выучить css", completed: false },
-    { id: "3", text: "выучить js", completed: false },
-    { id: "4", text: "выучить фреймворк", completed: false },
-    // { id: "5", text: "написать несколько учебных проектов", completed: false },
-    // { id: "6", text: "пройти собеседование", completed: true },
-    // { id: "7", text: "получить работу", completed: false }
+    // { id: "1", text: "выучить html", completed: true },
+    // { id: "2", text: "выучить css", completed: false },
+    // { id: "3", text: "выучить js", completed: false },
+    // { id: "4", text: "выучить фреймворк", completed: false },
+    // // { id: "5", text: "написать несколько учебных проектов", completed: false },
+    // // { id: "6", text: "пройти собеседование", completed: true },
+    // // { id: "7", text: "получить работу", completed: false }
 ];
 // Создание вложенности эллементов
 
@@ -37,35 +37,20 @@ function createListItem() {
     return li;
 
 };
-
 // массив задач
 function renderTasks() {
-
-    if (tasksList.length > 0) {
-        const ul = document.querySelector('.todo-list');
-        for (let i = 0; i < tasksList.length; i++) {
-            const li = createListItem();
-            ul.append(li);
-            li.id = tasksList[i].id;
-        };
-
-        const input = document.querySelectorAll('.toggle');
-        const label = document.querySelectorAll('.todo-list li label');
-        for (let i = 0; i < tasksList.length; i++) {
-            label[i].innerHTML = tasksList[i].text;
-            input[i].checked = tasksList[i].completed;
-        };
-    };
-
+    const ul = document.querySelector('.todo-list');
+    const li = createListItem();
+    ul.append(li);
     const input = document.querySelectorAll('.toggle');
     const label = document.querySelectorAll('.todo-list li label');
     for (let i = 0; i < tasksList.length; i++) {
+        li.id = tasksList[i].id
         label[i].innerHTML = tasksList[i].text;
         input[i].checked = tasksList[i].completed;
     };
+    console.log(ul);
 };
-
-
 function getid(arr) {
     if (arr.length > 0) {
         return arr[arr.length - 1]['id'];
@@ -73,28 +58,37 @@ function getid(arr) {
         return arr;
     };
 };
-
 function createNewTask() {
-    let id = getid(tasksList);
-    id++;
-    const task = {
-        id: "" + id,
-        text: "",
-        completed: false
-    };
-    tasksList.push(task);
+    let inputValueTask = document.querySelector('.new-todo');
+    inputValueTask.addEventListener('keydown', function (inter) {
+        if (inter.keyCode == 13) {
+            if(inputValueTask.value.length == 0) return;
+            let id = getid(tasksList);
+            id++;
+            const task = {
+                id: "" + id,
+                text: inputValueTask.value,
+                completed: false
+            };
+            tasksList.push(task);
+            renderTasks()
+            countActiveTasks()
+            inputValueTask.value = "";
+        };
+    });
 };
 function deleteTask() {
     const ul = document.querySelector('.todo-list');
-    ul.onclick = function (event) {
-        if (event.target.className != "destroy") return;
-        const li = event.target.closest('li');
-        const liId = li.id;
-        if (!li) return;
-        let task = tasksList.find(item => item.id == liId);
-        tasksList = tasksList.filter(t => t.id !== task.id);
-        li.remove();
-    };
+    ul.addEventListener('click', function(event) {
+        if (event.target.className == "destroy") {
+            const li = event.target.closest('li');
+            const liId = li.id;
+            li.remove();
+            let task = tasksList.find(item => item.id == liId);
+            tasksList = tasksList.filter(t => t.id !== task.id);
+        };
+
+    });
 };
 function toggleTask() {
     const ul = document.querySelector('.todo-list');
@@ -104,7 +98,7 @@ function toggleTask() {
             li.classList.add('completed');
         };
     };
-    ul.onclick = function (event) {
+    ul.addEventListener('click', function(event) {
         if (event.target.type != "checkbox") return;
         let li = event.target.closest('li');
         if (!li) return;
@@ -123,7 +117,7 @@ function toggleTask() {
             countActiveTasks()
             checkClearCompleted()
         };
-    };
+    });
 };
 function countActiveTasks() {
     let task = tasksList.filter(item => item.completed == false);
@@ -152,5 +146,12 @@ function checkClearCompleted() {
         clearButton.style.display = "none";
     } else {
         clearButton.style.display = "block";
-    };   
+    };
 };
+createNewTask()
+deleteTask()
+toggleTask()
+checkClearCompleted() 
+deleteCompleteTask()
+countActiveTasks()
+
